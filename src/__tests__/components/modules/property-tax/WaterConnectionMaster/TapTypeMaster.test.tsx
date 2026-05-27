9bff51cb67d3db25515cfd924e17c01baa782e21
Mamta-Patil-git <it.mamtapatil@sthapatya.in>
2026-05-27 13:07:12 +0530

    added

--- src/__tests__/components/modules/property-tax/WaterConnectionMaster/TapTypeMaster.test.tsx
+++ src/__tests__/components/modules/property-tax/WaterConnectionMaster/TapTypeMaster.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { IntlProvider } from "next-intl";
 import { TapTypeMaster } from "@/components/modules/property-tax/WaterConnectionMaster/TapTypeMaster";
 import { ConfirmProvider } from "@/components/common/ConfirmProvider";
@@ -13,6 +13,6 @@ describe("TapTypeMaster", () => {
         </ConfirmProvider>
       </IntlProvider>
     );
-    expect(document.getElementById("tap-type-page-size")).toBeInTheDocument();
+    expect(screen.getByRole("combobox")).toBeInTheDocument();
   });
 });
