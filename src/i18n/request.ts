65f543bf57c203ff7bb9ba9b7d037ade4c17fcb6
RiddhiShirsath <it.riddhishirsat@sthapatya.in>
2026-05-30 16:14:45 +0530

    Feature/update common details (#2120)
    * update common details UI chnages
    
    * resolved comments
    
    * resolved comments
    
    * resolved comments
    
    * resolved
    
    * resolved comments
    
    * updated
    
    ---------
    
    Co-authored-by: Riddhi Shirsat <Riddhi.Shirsat@sthapatya.com>

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -120,9 +120,6 @@ export default getRequestConfig(async ({ locale }) => {
       .then((m) => m.default || m),
     import(`./locales/${validatedLocale}/waterconnection.json`)
       .catch(() => ({}))
-      .then((m) => m.default || m),    
-    import(`./locales/${validatedLocale}/commonDetailsUpdate.json`)
-      .catch(() => ({}))
       .then((m) => m.default || m),
     import(`./locales/${validatedLocale}/waterConnectionMaster.json`)
       .catch(() => ({}))
