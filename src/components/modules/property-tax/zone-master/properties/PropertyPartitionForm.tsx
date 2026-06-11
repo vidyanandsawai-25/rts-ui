638e41d52834303fc7bce1346d8e1ef2e1aee72c
sujalscipl2025 <it.sujalkaware@sthapatya.in>
2026-06-11 16:15:29 +0530

    Added Validation for Minimum Owner name length and Fixed QA Point for Incremented by Stat (#2248)
    Added Validation for Minimum Owner name length and Fixed QA Point for Incremented by Stat

--- src/components/modules/property-tax/zone-master/properties/PropertyPartitionForm.tsx
+++ src/components/modules/property-tax/zone-master/properties/PropertyPartitionForm.tsx
@@ -382,6 +382,19 @@ export default function PropertyPartitionForm({
             visible={!!errors.mainPropertyId}
             type="error"
           />
+          {/* No properties available info */}
+          {!loadingBuildingList && propertyOptions.length === 0 && (
+            <div className="flex items-start gap-3 p-4 mt-3 bg-blue-50 border border-blue-200 rounded-lg">
+              <div className="w-5 h-5 mt-0.5 flex-shrink-0">
+                <svg className="w-5 h-5 text-blue-600" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <div className="flex-1">
+                <p className="text-sm font-medium text-blue-800">{t("partitionForm.helpText.noMainPropertiesFound")}</p>
+              </div>
+            </div>
+          )}
         </div>
 
         {/* Partition Type Tabs - Only show for Apartment categories */}
@@ -514,6 +527,7 @@ export default function PropertyPartitionForm({
         loading={loadingPreview}
         wingLetter={form.wingLetter}
         propertyNo={selectedProperty?.propertyNo}
+        incrementedBy={form.incrementedBy}
         taxZoneId={selectedProperty?.taxZoneId}
         wardId={selectedWard?.id}
         propertyTypeId={selectedProperty?.propertyTypeId ?? undefined}
