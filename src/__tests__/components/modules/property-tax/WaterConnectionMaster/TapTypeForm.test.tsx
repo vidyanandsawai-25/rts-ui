c1fc88741fda22ea468abae861de8b70eb4fd2c3
TanmayScipl <it.tanmaytarwade@sthapatya.in>
2026-07-11 11:15:30 +0530

    added
    added

--- src/__tests__/components/modules/property-tax/WaterConnectionMaster/TapTypeForm.test.tsx
+++ src/__tests__/components/modules/property-tax/WaterConnectionMaster/TapTypeForm.test.tsx
@@ -6,7 +6,17 @@ import { ConfirmProvider } from "@/components/common/ConfirmProvider";
 describe("TapTypeForm", () => {
   it("renders without crashing", () => {
     render(
-      <IntlProvider locale="en" messages={{}}>
+      <IntlProvider
+        locale="en"
+        messages={{
+          common: { save: "Save", cancel: "Cancel" },
+          waterConnectionMaster: { tapType: { title: "Tap Type" } }
+        }}
+        onError={(err) => {
+          if (err.code === 'MISSING_MESSAGE') return;
+          console.error(err);
+        }}
+      >
         <ConfirmProvider>
           <TapTypeForm id={null} />
         </ConfirmProvider>
