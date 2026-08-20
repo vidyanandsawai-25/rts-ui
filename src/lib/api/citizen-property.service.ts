703ca4224d49e657dfb009bfd19e133f607a49ed
HrishikeshPatekarZen <it.HrishikeshPatekar@sthapatya.in>
2026-08-20 19:41:29 +0530

    fix(api): correct API URL for fetching citizen property details

--- src/lib/api/citizen-property.service.ts
+++ src/lib/api/citizen-property.service.ts
@@ -18,7 +18,7 @@ export async function fetchCitizenPropertiesFromApi(
   value: string
 ): Promise<CitizenProperty[]> {
   try {
-    const url = `${process.env.AKOLA_CITIZEN_DETAILS_API_KEY}/PropertyTaxMicroService/PropertyTaxApi/Landing/GetCitizensDetails`;
+    const url = `${process.env.AKOLA_CITIZEN_PROPERTY_DETAILS_API_URL}/PropertyTaxMicroService/PropertyTaxApi/Landing/GetCitizensDetails`;
 
     const payload: any = {
       searchType,
